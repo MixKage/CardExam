4e0ff1403e4fa08f8dd77e226e51e91580a04389
Valerij <shishov265@gmail.com>
2022-08-07 17:06:35 +0300

    add: добавлена адаптивная вёрстка

---
+++ lib/login_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:cardexam/main_page';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final int _minWidht = 600;
+  @override
+  Widget build(context) => SafeArea(
+        child: Scaffold(body: LayoutBuilder(builder: (context, constraints) {
+          return AnimatedContainer(
+              duration: const Duration(milliseconds: 500),
+              color: Colors.lightGreen[200],
+              padding: constraints.maxWidth < _minWidht
+                  ? EdgeInsets.zero
+                  : const EdgeInsets.all(100.0),
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 25.0),
+                  constraints: const BoxConstraints(
+                    maxWidth: 600,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        FittedBox(
+                          fit: BoxFit.fitWidth,
+                          child: Text(
+                            "Welcome to the app, please log in",
+                            style: TextStyle(
+                                fontSize:
+                                    constraints.maxWidth < _minWidht ? 40 : 60),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              borderRadius: BorderRadius.circular(10)),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                labelText: "username",
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              borderRadius: BorderRadius.circular(10)),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8.0),
+                            child: TextField(
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                labelText: "password",
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ),
+                        ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxWidth:
+                                constraints.maxWidth < _minWidht ? 300 : 150,
+                            minWidth:
+                                constraints.maxWidth < _minWidht ? 250 : 150,
+                            maxHeight:
+                                constraints.maxWidth < _minWidht ? 100 : 50,
+                            minHeight:
+                                constraints.maxWidth < _minWidht ? 50 : 50,
+                          ),
+                          child: Expanded(
+                            child: RaisedButton(
+                                color: Colors.blue,
+                                child: Text("Log in",
+                                    style:
+                                        const TextStyle(color: Colors.white)),
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => HomePage()));
+                                }),
+                          ),
+                        )
+                      ]),
+                ),
+              ));
+        })),
+      );
+}
