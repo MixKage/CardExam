27399f7b512a829ef59d32dfd6b7ac2b312293f2
MixKage <shishov265@gmail.com>
2022-08-18 18:21:41 +0300

    fix: l_t_p: fin ui

--- lib/login_test_page.dart
+++ lib/login_test_page.dart
@@ -1,123 +1,135 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'image_network_path.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final widhtScreen = MediaQuery.of(context).size.width;
-    var usernameController = TextEditingController();
-    var passwordController = TextEditingController();
+  State<LoginPage> createState() => _LoginPageState();
+}
 
+class _LoginPageState extends State<LoginPage> {
+  late final TextEditingController usernameController = TextEditingController();
+  late final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final widthScreen = MediaQuery.of(context).size.width;
+    int maxWidth = 650;
     return Scaffold(
-      body: widhtScreen > 650
-          ? Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Flexible(
-                  flex: 4,
-                  child: Container(
-                    decoration: const BoxDecoration(
+      body: Builder(builder: (context) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            if (widthScreen > maxWidth)
+              Flexible(
+                flex: 4,
+                child: Container(
+                  decoration: const BoxDecoration(
                       image: DecorationImage(
                         fit: BoxFit.cover,
                         image: NetworkImage(pathImage),
                       ),
+                      color: Colors.amber),
+                ),
+              ),
+            Flexible(
+              flex: 3,
+              child: Container(
+                decoration: widthScreen > maxWidth
+                    ? const BoxDecoration(
+                        color: Colors.white,
+                        border: Border(
+                          left: BorderSide(
+                            color: Colors.orange,
+                            width: 5,
+                          ),
+                        ),
+                      )
+                    : const BoxDecoration(
+                        color: Colors.white,
+                      ),
+                child: Center(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 300),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Login',
+                            style: TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 50),
+                          TextField(
+                            controller: usernameController,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.person_sharp),
+                              hintText: "username",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          TextField(
+                            obscureText: true,
+                            controller: passwordController,
+                            decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.password),
+                              hintText: "password",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Expanded(
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Вход',
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Expanded(
+                                child: OutlinedButton(
+                                    onPressed: () {},
+                                    child: const Text(
+                                      'Регистрация',
+                                      textAlign: TextAlign.center,
+                                    )),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-                Flexible(
-                  flex: 3,
-                  child: loginContainer(
-                      context, usernameController, passwordController),
-                ),
-              ],
-            )
-          : loginContainer(context, usernameController, passwordController),
-    );
-  }
-
-  Widget loginContainer(
-      BuildContext context, usernameController, passwordController) {
-    return Container(
-      constraints: const BoxConstraints(
-        maxWidth: 650,
-        minWidth: 0,
-      ),
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          vertical: 30,
-          horizontal: 40,
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Login',
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
               ),
-            ),
-            const SizedBox(height: 50),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey[50],
-                  borderRadius: BorderRadius.circular(10)),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0),
-                child: TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.person_sharp),
-                    labelText: "username",
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.grey[50],
-                  borderRadius: BorderRadius.circular(10)),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0),
-                child: TextField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.password),
-                    labelText: "password",
-                    border: InputBorder.none,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                TextButton(
-                    onPressed: () {},
-                    child: const Text(
-                      'Забыли пароль?',
-                      style: TextStyle(decoration: TextDecoration.underline),
-                    )),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: const Text('вход'),
-                ),
-              ],
             ),
           ],
-        ),
-      ),
+        );
+      }),
     );
   }
 }
