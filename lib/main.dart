3c989399c65b8119c641d232e79cf1984289ce5a
MixKage <shishov265@gmail.com>
2022-09-02 16:58:50 +0300

    add: подключение клиента к серверу

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 
 Future<String> fetchAlbum() async {
   final response =
-      await http.get(Uri.parse('http://192.168.1.220:5013/api/Text'));
+      await http.get(Uri.parse('http://192.168.1.225:5293/WeatherForecast'));
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
@@ -69,20 +69,29 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Fetch Data Example'),
         ),
-        body: Center(
-          child: FutureBuilder<String>(
-            future: text,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.toString());
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FutureBuilder<String>(
+              future: text,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.toString());
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
 
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
+                // By default, show a loading spinner.
+                return const CircularProgressIndicator();
+              },
+            ),
+            RaisedButton(onPressed: () {
+              setState(() {
+                text = fetchAlbum();
+              });
+            }),
+          ],
         ),
       ),
     );
